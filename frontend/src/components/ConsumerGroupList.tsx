afb2c86cfe52043abdb156770ecb45cb00e60268
BillohMBarry <alhajibillohbarrie@gamil.com>
2025-05-01 20:54:50 +0000

    fix(ui): make background blue on focus and disable conflicting css

--- frontend/src/components/ConsumerGroupList.tsx
+++ frontend/src/components/ConsumerGroupList.tsx
@@ -76,7 +76,7 @@ const ConsumerGroupList: React.FC = () => {
                             to="/topics" 
                             className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium focus:bg-blue-600 focus:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                         >
-                            View Topicss
+                            View Topics
                         </Link>
                     </div>
                 </div>
